00786cf8a52344712822213a1e0c08e69e336cfb
Abhishek Girish <agirish@apache.org>
2021-10-07 19:21:15 -0700

    Preparing for Oct Preview Drop

--- dockerfiles/spark-hs-3.1.1/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --no-cache --network=host -t apache-spark-hs .
-
-VERSION="3.1.1"
-
-tag=$(date +%Y%m%d%H%M)
-docker tag apache-spark-hs gcr.io/mapr-252711/apache-spark-hs-${VERSION}:$tag
-docker push gcr.io/mapr-252711/apache-spark-hs-${VERSION}:$tag
