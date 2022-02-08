d56f885fbedb38489c790be3492239d63d008ec2
alex.navara <aleksander.navara@hpe.com>
2022-02-08 16:27:50 +0200

    Add spark+GPU example

---
+++ examples/spark-3.1.2/apps/src/main/scala/com/hpe/examples/spark/sql/SqlOnGpuExample.scala
@@ -0,0 +1,24 @@
+package com.hpe.examples.spark.sql
+
+import org.apache.spark.sql.SparkSession
+
+class SqlOnGpuExample {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .getOrCreate()
+    val sc = spark.sparkContext
+
+    import spark.implicits._
+
+    val viewName = "df"
+    val df = sc.parallelize(Seq(1, 2, 3)).toDF("value")
+    df.createOrReplaceTempView(viewName)
+
+    spark.sql(s"SELECT value FROM $viewName WHERE value <>1").explain()
+    spark.sql(s"SELECT value FROM $viewName WHERE value <>1").show()
+
+    spark.stop()
+  }
+
+}
