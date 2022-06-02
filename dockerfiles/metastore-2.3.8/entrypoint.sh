4c3895a856425f5cd4e75a7a3cb286f054fe85c5
anavara <aleksander.navara@hpe.com>
2022-06-02 17:00:47 +0300

    Prepare ere-5.4.2 branch - remove unreleased components

--- dockerfiles/metastore-2.3.8/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-/opt/hive/bin/schematool -initSchema -dbType mysql
-/opt/hive/bin/hive --service metastore
--- examples/spark-2.4.4/README.md
+++
@@ -1,2 +0,0 @@
-Examples for Ezmeral Spark on Kubernetes v2.4.4
-Compatible with ECP v5.3.0
