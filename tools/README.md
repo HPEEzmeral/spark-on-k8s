feaa474bc40639c3f3429c6a15a594e32f477d5c
mayankgarg23 <mayankgarg.jpr@gmail.com>
2022-07-25 13:38:50 -0700

    fetch and download images

---
+++ tools/README.md
@@ -0,0 +1,42 @@
+# Script to fetch image tags and download these images from GCR repository on local setup
+
+### Prerequisites:
+1. Install minikube on your local setup with docker runtime and appropriate configs
+```
+minikube config set cpus 6
+minikube config set memory 64g
+minikube config set disk-size 64g
+```
+2. Start the minikube single node setup
+```
+minikube start --driver=hyperkit --container-runtime=docker
+```
+3. Set up docker env variables
+```
+minikube docker-env
+eval $(minikube -p minikube docker-env) 
+```
+NOTE: kubectl now has been configured to point to the minikube setup automatically.
+NOTE: docker now has been configured to be used in current session window. 
+
+#### Store image name and tag in a file
+Run the following command from fetch_tag directory to execute the fetch tag script
+```
+./fetch_tag.sh
+```
+
+#### Download images from the file and save them to a tar archive
+Run the following command from download_image directory to execute the image download script
+```
+./download-images.sh --image-list ../imagelist.txt
+```
+
+### Copy this images.tar.gz to desired location
+
+###  Unload the images.tar.gz to docker as images
+Use the following command to get the docker images loaded from .tar.gz
+```
+docker load < images.tar.gz
+```
+
+
---
+++ tools/fetch_tag/fetch_tag.py
@@ -0,0 +1,108 @@
+import argparse
+import os
+import re
+import sys
+
+import requests
+import yaml
+
+class FetchTag(object):
+    USER = "_token"
+
+    def __init__(self):
+        cur_dir = os.path.dirname(__file__)
+
+        # will be a dict of <string, list>
+        self.chart_files = dict()
+        self.example_files = dict()
+
+        self.re_pattern = re.compile("([a-zA-Z_-]+-\\d+.\\d+.*):(.+)")
+
+        self.charts_dir = os.path.abspath(os.path.join(cur_dir, "../../charts"))
+        self.examples_dir = os.path.abspath(os.path.join(cur_dir, "../../examples"))
+        self.imgtxt_file = os.path.abspath(os.path.join(cur_dir, "../imagelist.txt"))
+    def run(self):
+        self.get_tags_from_charts_yaml(self.charts_dir, self.chart_files, ["livy-0.8.0", "metastore-3.1.1", "mysql-operator", "spark-3.1.1", "spark-hs-3.1.1"])
+        self.get_tags_from_examples_yaml(self.examples_dir, self.example_files, ["spark-2.4.4", "spark-3.1.1"])
+        print(self.chart_files)
+        print(self.example_files)
+        with open(self.imgtxt_file, "w") as file:
+            for key in self.chart_files:
+                for k, v in self.chart_files[key].items():
+                    file.write(k + ":" + v)
+                    file.write("\n")
+            for key in self.example_files:
+                for k, v in self.example_files[key].items():
+                    file.write(k + ":" + v)
+                    file.write("\n")
+    
+    def get_tags_from_charts_yaml(self, start_dir, process_dict, ignore_dirs=None):
+        if ignore_dirs is None:
+            ignore_dirs = []
+        for dirpath, dirnames, filenames in os.walk(start_dir):
+            process = True
+            for a_file in filenames:
+                if not a_file.lower().endswith("values.yaml"):
+                    continue
+                for ignore_dir in ignore_dirs:
+                    if ignore_dir in dirpath:
+                        process = False
+
+                if not process:
+                    continue
+
+                full_file = os.path.join(dirpath, a_file)
+                with open(full_file, "r") as file:
+                    try:
+                        contents = yaml.safe_load(file)
+                        append_dict = dict()
+                        key = contents['image']['imageName']
+                        value = contents['image']['tag']
+                        if value not in append_dict.values():
+                            append_dict[key] = value
+                        append_dict[key] = value
+                        if len(append_dict) > 0:
+                                process_dict[full_file] = append_dict
+                    except yaml.YAMLError as exc:
+                        print(exc)
+
+    def get_tags_from_examples_yaml(self, start_dir, process_dict, ignore_dirs=None):
+        if ignore_dirs is None:
+            ignore_dirs = []
+        for dirpath, dirnames, filenames in os.walk(start_dir):
+            process = True
+            for a_file in filenames:
+                if not a_file.lower().endswith(".yaml"):
+                    continue
+                for ignore_dir in ignore_dirs:
+                    if ignore_dir in dirpath:
+                        process = False
+
+                if not process:
+                    continue
+
+                full_file = os.path.join(dirpath, a_file)
+                with open(full_file, "r") as file:
+                    contents = file.read()
+                    found = self.re_pattern.findall(contents)
+
+                    a_len = len(found)
+                    if a_len > 0:
+                        append_dict = dict()
+                        for found_dict in found:
+                            key = found_dict[0]
+                            value = found_dict[1].replace('"', '')
+                            if key.find(">") >= 0:
+                                continue
+                            if "/" in key:
+                                key = key[key.rindex("/") + 1:]
+                            if key.find("mapr-252711") >= 0:
+                                continue
+                            append_dict[key] = value
+                        if len(append_dict) > 0:
+                            if append_dict not in process_dict.values():
+                                process_dict[full_file] = append_dict
+
+if __name__ == '__main__':
+    fetch_tag = FetchTag()
+    fetch_tag.run()
