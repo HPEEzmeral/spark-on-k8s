efec79f3041480af8c838012e7e0845ef81d5108
yaroslav <yaroslav.chernysh@hpe.com>
2022-12-20 18:16:26 +0200

    EZAF-422 Do not remove PVC in ranger when uninstalling chart by helm

--- charts/ranger/ranger-chart/files/full-cleanup.sh
+++ charts/ranger/ranger-chart/files/full-cleanup.sh
@@ -6,8 +6,6 @@ echo NameSpace: $ns
 
 echo helm uninstall ranger -n $ns
 helm uninstall ranger -n $ns
-echo kubectl delete pvc --all -n $ns
-kubectl delete pvc --all -n $ns
 
 echo kubectl get all -n $ns
 kubectl get all -n $ns
